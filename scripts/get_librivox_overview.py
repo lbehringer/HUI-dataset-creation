b83c6213cfd56521dd752e1be471e1384f376c4a
lbehringer <lyonel.behringer@hotmail.de>
2023-11-05 16:53:20 +0100

    add option for custom request URL, recognize gutenberg source URLs without www

--- scripts/get_librivox_overview.py
+++ scripts/get_librivox_overview.py
@@ -6,7 +6,8 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--database_path", default=None, help="Set directory where overview file should be created.")
+    parser.add_argument("-d", "--database_path", type=str, default=None, help="Set directory where overview file should be created.")
+    parser.add_argument ("--request_url", type=str, default=None, help="Set custom request URL for metadata retrieval.")
     args = parser.parse_args()
 
     if args.database_path:
@@ -15,6 +16,7 @@ if __name__ == "__main__":
         repo_root_path = os.path.join(os.path.dirname(__file__), "..")
         database_dir = "database"
         database_path = os.path.join(repo_root_path, database_dir)
+    request_url = args.request_url
 
     os.makedirs(database_path, exist_ok=True)    
 
@@ -27,7 +29,8 @@ if __name__ == "__main__":
             'chapterPath': ''
         },
         'step0_Overview': {
-            'savePath': step0Path
+            'savePath': step0Path,
+            "requestUrl": request_url
         }
     }
     DependencyInjection(config).step0_Overview.run()
