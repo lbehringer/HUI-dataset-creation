9d659a17ec6a5caa34fa46b5bd38594549f108f0
lbehringer <lyonel.behringer@hotmail.de>
2023-11-16 23:24:22 +0100

    :construction: added SNR calculation, but need to deal with noise-removal algorithms skewing SNR values

--- huiAudioCorpus/workflows/createDatasetWorkflow/QA2_HifiSNR.py
+++ huiAudioCorpus/workflows/createDatasetWorkflow/QA2_HifiSNR.py
@@ -13,7 +13,7 @@ import librosa
 from scipy.signal import butter, lfilter
 import matplotlib.pyplot as plt
 import soundfile as sf
-
+from copy import deepcopy
 
 class QA2_HifiSNR:
     def __init__(
@@ -46,16 +46,39 @@ class QA2_HifiSNR:
         audios = self.audio_persistenz.loadAll(duration=self.seconds_to_analyze)
         for idx, audio in enumerate(audios):
             audio = self.audio_loudness_transformer.transform(audio)
-            audio.speech_timestamps = self.apply_vad(audio)
-            audio.silence_timestamps = self.get_silence_timestamps(audio)
+            speech_timestamps, silence_timestamps = self.apply_vad(audio)
             audio = self.set_x_seconds_id(audio, self.book_name, idx+1, self.seconds_to_analyze)
-            # TODO: compute SNR for specified frequency bands
+            frequency_bands = [(100, 1000), (300, 4000), (4000, 10000), (10000, 15000)]
+
+            for lower_freq, upper_freq in frequency_bands:
+                print(f"SNR for freq band {lower_freq}-{upper_freq}:")
+                print(self.get_snr(
+                    audio=audio, 
+                    speech_timestamps=speech_timestamps, 
+                    silence_timestamps=silence_timestamps, 
+                    lower_freq_threshold=lower_freq, 
+                    upper_freq_threshold=upper_freq,
+                    ))
+
+            # TODO: Find a way to identify whether a noise-removal algorithm has been applied
+
             self.audio_persistenz.save(audio)
+            
+            upsample_factor = audio.samplingRate / self.target_sr
+            speech_signal_segments = [audio.timeSeries[int(ts['start']*upsample_factor):int(ts['end']*upsample_factor)] for ts in speech_timestamps]
+            silence_signal_segments =  [audio.timeSeries[int(ts['start']*upsample_factor):int(ts['end']*upsample_factor)] for ts in silence_timestamps]
+            speech_signal = np.concatenate(speech_signal_segments)
+            silence_signal = np.concatenate(silence_signal_segments)
+            speech_audio = deepcopy(audio)
+            silence_audio = deepcopy(audio)
+            speech_audio.timeSeries, speech_audio.id = speech_signal, audio.id+"speech"
+            silence_audio.timeSeries, silence_audio.id = silence_signal, audio.id+"silence"
+            self.audio_persistenz.save(speech_audio)
+            self.audio_persistenz.save(silence_audio)
                 
 
             # TODO: generate 4 timeSeries, one for each frequency band that we want to analyze
             # TODO: Is it better to just compute the mel spectrogram, "cut off" all frequencies outside the passband, and then do an inverse mel transform and an inverse STFT?
-            print(audio.samplingRate)
             # design band-pass Butterworth filter
             lowcut = 300
             highcut = 4000
@@ -124,32 +147,48 @@ class QA2_HifiSNR:
         Takes a loaded Audio object as input and splits it into speech chunks.
         Returns
         -------
-        speeches: list of dicts
-        list containing ends and beginnings of speech chunks (samples or seconds based on return_seconds)"""
+        speech_timestamps: list of dicts
+            list containing ends and beginnings of speech chunks (samples or seconds based on return_seconds)
+        silence_timestamps: list of dicts
+            list containing ends and beginning of each silence chunk
+        """
         
         audio = self.audio_sr_transformer.transform(audio=audio)
         wav = torch.from_numpy(audio.timeSeries)
-        with torch.no_grad():
-            # wav = self.read_audio(audio, sampling_rate=self.target_sr)
-            return self.get_speech_timestamps(
-                wav,
-                self.vad_model,
-                threshold=0.5,  # speech prob threshold
-                sampling_rate=self.target_sr,  # sample rate
-                min_speech_duration_ms=300,  # min speech duration in ms
-                # max_speech_duration_s=20,  # max speech duration in seconds
-                min_silence_duration_ms=400,  # min silence duration
-                window_size_samples=512,  # window size
-                speech_pad_ms=150,  # speech pad ms
-            )       
+        min_silence_duration_ms = 150
+        min_speech_duration_ms = 250
+        # get speech_timestamps with a config that will yield at least one speech chunk and at least one silence chunk
+        # we assume that a very short chunk (~10ms) for either speech or silence will result in a too low SNR so the sample will be filtered out        
+        while min_speech_duration_ms > 10 and min_silence_duration_ms > 10:        
+            with torch.no_grad():
+                speech_timestamps = self.get_speech_timestamps(
+                        wav,
+                        self.vad_model,
+                        threshold=0.5,  # speech prob threshold
+                        sampling_rate=self.target_sr,  # sample rate
+                        min_speech_duration_ms=min_speech_duration_ms,  # min speech duration in ms
+                        max_speech_duration_s=float('inf'),  # max speech duration in seconds
+                        min_silence_duration_ms=min_silence_duration_ms,  # min silence duration
+                        window_size_samples=512,  # window size
+                        speech_pad_ms=30,  # speech pad ms
+                    )
+                
+            silence_timestamps = self.get_silence_timestamps(speech_timestamps)
+            if len(speech_timestamps) == 0:
+                min_speech_duration_ms = int(min_speech_duration_ms / 2)
+            if len(silence_timestamps) == 0:
+                min_silence_duration_ms = int(min_silence_duration_ms / 2)
+            else:
+                return speech_timestamps, silence_timestamps
+        return
         
-    def get_silence_timestamps(self, audio: Audio):
-        """Takes an Audio object as input which has the instance attribute `speech_timestamps`.
+    def get_silence_timestamps(self, speech_timestamps: list):
+        """Takes as input a list of dicts `speech_timestamps`, containing "start" and "end" of each speech segment.
         Computes the complementary silence_timestamps.
         Returns a list of dicts containing start and end of each silence segments."""
         silence_timestamps = []
         start = 0
-        for idx, speech_chunk in enumerate(audio.speech_timestamps[:-1]):
+        for idx, speech_chunk in enumerate(speech_timestamps[:-1]):
             if idx == 0:
                 start = speech_chunk["end"]
             else:
@@ -158,32 +197,40 @@ class QA2_HifiSNR:
                 start = speech_chunk["end"]
         return silence_timestamps
 
-    def get_snr(self, audio: Audio, speech_timestamps: list, silence_timestamps: list):
-        """Estimates the signal-plus-noise power in speech segments and the noise power in silence segments, then computes the SNR."""
+    def get_snr(self, audio: Audio, speech_timestamps: list, silence_timestamps: list, lower_freq_threshold: float, upper_freq_threshold: float):
+        """Estimates the signal-plus-noise power in speech segments and the noise power in silence segments, then computes the signal-to-noise ratio (SNR)."""
         # get speech-only audio and silence-only audio
-        speech_audio_segments = [audio.timeSeries[ts['start']:ts['end']] for ts in speech_timestamps]
-        silence_audio_segments =  [audio.timeSeries[ts['start']:ts['end']] for ts in silence_timestamps]
-        speech_audio = np.concatenate(speech_audio_segments)
-        silence_audio = np.concatenate(silence_audio_segments)
+        # upsample timestamps to match audio's sampling rate (factor is 1 if they already match)
+        upsample_factor = audio.samplingRate / self.target_sr
+        speech_signal_segments = [audio.timeSeries[int(ts['start']*upsample_factor):int(ts['end']*upsample_factor)] for ts in speech_timestamps]
+        silence_signal_segments =  [audio.timeSeries[int(ts['start']*upsample_factor):int(ts['end']*upsample_factor)] for ts in silence_timestamps]
+        speech_signal = np.concatenate(speech_signal_segments)
+        silence_signal = np.concatenate(silence_signal_segments)
 
         # get signal-plus-noise power in speech audio
-        speech_power = self.get_average_power(speech_audio)
+        speech_power = self.get_average_power(speech_signal, audio.samplingRate, lower_freq_threshold, upper_freq_threshold)
         # get noise power in silence audio
-        silence_power = self.get_average_power(silence_audio)
+        silence_power = self.get_average_power(silence_signal, audio.samplingRate, lower_freq_threshold, upper_freq_threshold)
         # compute SNR
         return speech_power / silence_power
 
-    def get_average_power(self, y: np.ndarray):
+    def get_average_power(self, y: np.ndarray, sr: int, lower_freq_threshold: float, upper_freq_threshold: float):
         """Calculates the average power of an audio signal.
         Returns
         -------
         average_power (float)
         """
+        # compute STFT
         stft_result = librosa.stft(y)
-        magnitude_spec = np.abs(stft_result)
+        # compute frequency bins corresponding to STFT
+        freq_bins = librosa.fft_frequencies(sr=sr)
+        # find the indices corresponding to the chosen frequency band (between lower and upper threshold)
+        band_indices = np.where((freq_bins >= lower_freq_threshold) & (freq_bins <= upper_freq_threshold))[0]
+        # extract the magnitude spectrum for the chosen frequency band
+        magnitude_spec = np.abs(stft_result[band_indices, :])
         power_spec = magnitude_spec**2
         power_across_frequency_bins = np.sum(power_spec, axis=0)
         total_power = np.sum(power_across_frequency_bins)
-        duration = len(y) / self.target_sr
+        duration = len(y) / sr
         average_power = total_power / duration
         return average_power
